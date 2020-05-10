6a5ce6857a10ab867e555c82dc9820f86e037580
rekad97 <reka.domjan@gmail.com>
2020-05-10 13:02:15 +0200

    first bug

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.spaceship;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -36,7 +37,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+    Random generator = SecureRandom.getInstanceStrong();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
